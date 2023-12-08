c03f1bbed0c61e80daf38427173fe3abe6c38ffa
only1tarunno <tarunno281@gmail.com>
2023-12-08 13:30:09 +0600

    rating page create

--- src/pages/Dashboard/UserPages/StarRating/StarRating.jsx
+++ src/pages/Dashboard/UserPages/StarRating/StarRating.jsx
@@ -29,7 +29,7 @@ const StarRating = () => {
       </div>
       <div>
         {products?.length ? (
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
             {products?.map((product, index) => (
               <RatingproductCard
                 key={index}
--- src/pages/Dashboard/UserPages/StarRating/ratingproductCard.jsx
+++ src/pages/Dashboard/UserPages/StarRating/ratingproductCard.jsx
@@ -1,25 +1,91 @@
+import { Rating } from "@smastrom/react-rating";
+import { useState } from "react";
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { useQuery } from "@tanstack/react-query";
+
 /* eslint-disable react/prop-types */
 const RatingproductCard = ({ product }) => {
-  console.log(product);
   const { _id, thumbnail_url, title } = product || {};
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [rating, setRating] = useState(4);
+  const { data: reviewData = {}, refetch } = useQuery({
+    queryKey: ["getReviewDatafrom", _id],
+    enabled: !!_id,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/review/${_id}`);
+      return res.data;
+    },
+  });
+  // console.log(product);
+
+  const handleRatings = async (e) => {
+    e.preventDefault();
+
+    const review = {
+      name: user.displayName,
+      product_id: _id,
+      value: rating,
+      email: user.email,
+      comment: e.target.feedback.value,
+      time: new Date(),
+    };
+
+    const res = await axiosSecure.post("/review", review);
+    refetch();
+    if (res.data.email) {
+      Swal.fire({
+        icon: "success",
+        title: "Thank you for your feedback",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
   return (
     <>
-      <div className="card bg-base-100 shadow-sm rounded">
-        <figure>
-          <img
-            src={thumbnail_url}
-            className="w-full object-cover"
-            alt={title}
-          />
+      <div className="card lg:card-side bg-base-100  shadow-xl">
+        <figure className="w-full  lg:w-1/3">
+          <img src={thumbnail_url} className="w-full h-full" alt={title} />
         </figure>
-        <div className="card-body">
-          <h2 className="text-lg text-center">{title}</h2>
+        <div className="card-body justify-center">
+          <h2 className="card-title">{title}</h2>
 
-          <div className="card-actions justify-center">
-            <button className="btn bg-[#f76b6a] border-[#f76b6a] px-8 rounded  hover:bg-[#4c5161] hover:border-[#4c5161] text-white font-medium uppercase">
-              Rate
-            </button>
-          </div>
+          {reviewData.success ? (
+            <div>
+              <p className="italic">Your review:</p>
+              <p>{reviewData.foundReview.comment}</p>
+
+              <Rating
+                style={{ maxWidth: 120 }}
+                value={reviewData.foundReview.value}
+                readOnly
+              />
+            </div>
+          ) : (
+            <form onSubmit={handleRatings}>
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={rating}
+                onChange={setRating}
+                isRequired
+              />
+              <textarea
+                placeholder="Message"
+                required
+                name="feedback"
+                className="border border-[#999] rounded p-2 w-full my-5"
+                id=""
+                rows="4"
+              ></textarea>
+              <button className="btn bg-[#f76b6a] border-[#f76b6a] px-8 rounded  hover:bg-[#4c5161] hover:border-[#4c5161] text-white font-medium uppercase">
+                Rate
+              </button>
+            </form>
+          )}
         </div>
       </div>
     </>
