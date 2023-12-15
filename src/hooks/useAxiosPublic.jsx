ae5c9181a24c77f8ba95d6e11ba2184afe1fe58a
only1tarunno <tarunno281@gmail.com>
2023-12-15 19:52:20 +0600

    add server live link

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://shop-server-chi.vercel.app",
 });
 
 const useAxiosPublic = () => {
